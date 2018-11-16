1d5e2f4b73d9b7010159711cecb6d3898239eb9c
shadowfox3735 <normanpan180@gmail.com>
2018-11-16 02:18:14 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,6 +59,8 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     holiday.each do |holiday, supplies|
       puts "  #{holiday.to_s.split("_").map {|i| i.capitalize}.join(" ")}: #{supplies.join(", ")}"
+    end
+  end
 end
 
 def all_holidays_with_bbq(holiday_hash)
